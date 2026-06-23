8b16b575c2e523981535db1ca4ec5bcbf343df06
Hitesh Shyamchand Bangade <info@shyamkasturiproductions.com>
2026-06-23 11:13:10 +0530

    fix: prisma client edge import

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,7 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client/edge"
 import { PrismaPg } from "@prisma/adapter-pg"
 
 const connectionString = process.env.DATABASE_URL!
-
 const adapter = new PrismaPg({ connectionString })
 
 declare global {
