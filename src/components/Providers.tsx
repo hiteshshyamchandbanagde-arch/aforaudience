faa53db5c34107bbcb3ebb94f7b1a856908adc05
hiteshshyamchandbanagde-arch <hiteshshyamchandbanagde@gmail.com>
2026-07-26 16:10:03 +0530

    refactor: use IdleTimeoutGuard instead of AdminIdleGuard

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,14 +2,14 @@
 
 import { SessionProvider } from "next-auth/react"
 import SessionGuard from "@/components/SessionGuard"
-import AdminIdleGuard from "@/components/AdminIdleGuard"
+import IdleTimeoutGuard from "@/components/IdleTimeoutGuard"
 import { ToastProvider } from "@/components/Toast"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <SessionGuard />
-      <AdminIdleGuard />
+      <IdleTimeoutGuard />
       <ToastProvider>{children}</ToastProvider>
     </SessionProvider>
   )
